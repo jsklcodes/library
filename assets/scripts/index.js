8397f469b5d84648dd314febee73c47c24bacb9c
Kallyne Layane <hellookallyne@gmail.com>
2023-07-21 14:18:36 -0300

    Add functionality to add books

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,3 +12,54 @@ function listenClickEvent(element, event) {
 
 listenClickEvent(openModalButton, () => toggleModalVisibility('add'));
 listenClickEvent(closeModalButton, () => toggleModalVisibility('remove'));
+
+const addBookForm = document.querySelector('#add-book-form');
+const library = document.querySelector('#library');
+
+let myLibrary = [];
+
+let id = myLibrary.length;
+
+function Book(title, author, pages, read) {
+  this.id = id++;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function removeLibraryChildrenExceptFirst() {
+  const children = library.children;
+
+  for (let i = 1; i < children.length; i++) {
+    children[i].remove();
+  }
+}
+
+function addBookToLibrary(book) {
+  removeLibraryChildrenExceptFirst();
+  myLibrary.push(book);
+  console.log(myLibrary);
+}
+
+addBookForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const [_, titleInput, authorInput, pagesInput, statusInput] =
+    event.target.elements;
+
+  const book = new Book(
+    titleInput.value,
+    authorInput.value,
+    pagesInput.value,
+    statusInput.checked
+  );
+
+  addBookToLibrary(book);
+  toggleModalVisibility('remove');
+
+  titleInput.value = '';
+  authorInput.value = '';
+  pagesInput.value = '';
+  statusInput.checked = true;
+});
